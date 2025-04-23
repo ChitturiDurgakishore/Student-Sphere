c28b542da913d045691267eedff811039cac343d
Durga Kishore <chitturidurgakishore@gmail.com>
2025-04-23 09:29:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Replace these with your actual credentials
 const clientID = "479563869562-d4frmm9dfvajlv2ntb4u9i8vm2jmuthr.apps.googleusercontent.com"; // Replace this
-const sheetsAPIUrl = "https://script.google.com/macros/s/AKfycbwlsUi-EI9EAMRaCnR3Lbt4D9SDSiy58o_GIqWFf6iLemmI7Dr5CBE0jX9BbBvyBy0/exec"; // Replace this
+const sheetsAPIUrl = "https://script.google.com/macros/s/AKfycbzXuMHzJjTQk4VOuat-VxoBVbNYvyNetlM5t7aefyWg5X6DO10r0s4a9xVzHPSQOww/exec"; // Replace this
 let accessToken = null;
 let userName = "";
 
